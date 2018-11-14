1cf4f302d076ac51d4effd0b59a101f5da4137a3
cutiko <cutiko@gmail.com>
2018-11-14 09:49:40 -0300

    FIX: layout manager was not working

--- app/src/main/java/cl/pingon/cotizador/views/main/machines/CommentsFragment.java
+++ app/src/main/java/cl/pingon/cotizador/views/main/machines/CommentsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,6 +38,7 @@ public class CommentsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         recyclerView = view.findViewById(R.id.commentsRv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         Machines machines = (Machines) getActivity().getIntent().getSerializableExtra(MACHINES_DETAILS);
 
